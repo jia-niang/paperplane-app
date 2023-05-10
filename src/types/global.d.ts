15c4745a436ec58e88370d2de30d67f4aa647eb3
jia-niang <1@paperplane.cc>
2023-05-10 15:35:44 +0800

    feat(types/global): 新增 TypeofPromise 类型工具 [ci skip]

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -5,8 +5,11 @@ type RC = ReturnType<React.FC>
 interface IProps {
   style?: React.CSSProperties
   className?: string
-  children?: ReactNode
+  children?: React.ReactNode
 }
 
 /** 获取数组的类型 */
 type TypeofArray<T extends any[]> = T extends (infer U)[] ? U : never
+
+/** 获取 Promise 的类型 */
+type TypeofPromise<T extends Promise<any>> = T extends Promise<infer U> ? U : never
