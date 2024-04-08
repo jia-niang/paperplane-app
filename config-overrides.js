c7558c56558475d082eb2d393ac3914b723756f2
jia-niang <1@paperplane.cc>
2024-04-09 04:13:48 +0800

    chore(app): 现在可以上传资源到 CDN

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,10 @@ const {
   addBabelPlugin,
   addLessLoader,
   addBabelPreset,
+  addWebpackPlugin,
 } = require('customize-cra')
+const S3Plugin = require('webpack-s3-plugin')
+const noop = require('lodash/identity')
 
 module.exports = {
   webpack: override(
@@ -35,7 +38,30 @@ module.exports = {
 
     addBabelPlugin(['@emotion']),
 
-    addBabelPreset(['@emotion/babel-preset-css-prop'])
+    addBabelPreset(['@emotion/babel-preset-css-prop']),
+
+    process.env.COS_SECRET_ID && process.env.COS_SECRET_KEY
+      ? addWebpackPlugin(
+          new S3Plugin({
+            exclude: /.*\.html$/,
+            basePath: 'paperplane-app',
+            s3Options: {
+              accessKeyId: process.env.COS_SECRET_ID,
+              secretAccessKey: process.env.COS_SECRET_KEY,
+              region: 'ap-hongkong',
+              endpoint: 'https://cos.ap-hongkong.myqcloud.com',
+              apiVersion: '2006-03-01',
+            },
+            s3UploadOptions: {
+              Bucket: 'paperplane-cdn-1253277322',
+            },
+            cdnizerOptions: {
+              defaultCDNBase: 'https://cdn.paperplane.cc/paperplane-app',
+            },
+            progress: false,
+          })
+        )
+      : noop
   ),
 
   devServer: overrideDevServer(devServerConfig => ({
